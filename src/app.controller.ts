24ce3ce7c0ae1b9301628c1bfe9537c3ec867ed7
Victor Carvalho <victorcarvalhosp@gmail.com>
2020-12-31 15:14:44 -0300

    Basic authentication example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  findAll(): string {
+    return 'This action returns all cats';
   }
 }
